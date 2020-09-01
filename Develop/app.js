ca263b04c8178bef55709772dec59212e09de172
Ahmed Sheikh <ahsheikh.a@gmail.com>
2020-09-01 03:08:22 -0400

    ran first build

--- Develop/app.js
+++ Develop/app.js
@@ -4,13 +4,24 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const questions = require("./utils/questions");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
+const promptQuestions = (type) => {
+    return inquirer.prompt(questions[type]);
+};
+
+const startApp = () => {
+    console.log("Please build your team.");
+};
+
+startApp();
 
 
 // Write code to use inquirer to gather information about the development team members,
